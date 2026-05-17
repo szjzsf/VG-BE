ecff2240e1f85e96159737a34d68c3e8e6621a87
szjzsf <90899903+szjzsf@users.noreply.github.com>
2026-05-17 14:28:58 +0200

    halak

---
+++ Halak/HalDTO.cs
@@ -0,0 +1,9 @@
+namespace HalakAPI.DTO
+{
+    public class HalDTO
+    {
+        public string halFaj { get; set; }
+        public decimal halMerete { get; set; }
+        public string toNeve { get; set; }
+    }
+}
